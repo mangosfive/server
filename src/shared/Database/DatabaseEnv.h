9a23d6f9d13f246ca26349af2c3834538c35e1dc
antz <antz@cix.co.uk>
2021-02-24 15:05:09 +0000

    Added files needed to realm building

---
+++ src/shared/Database/DatabaseEnv.h
@@ -0,0 +1,51 @@
+/**
+ * MaNGOS is a full featured server for World of Warcraft, supporting
+ * the following clients: 1.12.x, 2.4.3, 3.3.5a, 4.3.4a and 5.4.8
+ *
+ * Copyright (C) 2005-2021 MaNGOS <https://getmangos.eu>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * World of Warcraft, and all World of Warcraft or Warcraft art, images,
+ * and lore are copyrighted by Blizzard Entertainment, Inc.
+ */
+
+#if !defined(DATABASEENV_H)
+#define DATABASEENV_H
+
+#include "Common/Common.h"
+#include "Log/Log.h"
+#include "Utilities/Errors.h"
+
+#include "Database/Field.h"
+#include "Database/QueryResult.h"
+
+#include "Database/QueryResultMysql.h"
+#include "Database/Database.h"
+#include "Database/DatabaseMysql.h"
+/**
+ * @brief
+ *
+ */
+typedef DatabaseMysql DatabaseType;
+#define _LIKE_           "LIKE"
+#define _CONCAT3_(A,B,C) "CONCAT( " A " , " B " , " C " )"
+#define _OFFSET_         "LIMIT %d,1"
+
+extern DatabaseType WorldDatabase; /**< TODO */
+extern DatabaseType CharacterDatabase; /**< TODO */
+extern DatabaseType LoginDatabase; /**< TODO */
+
+#endif
